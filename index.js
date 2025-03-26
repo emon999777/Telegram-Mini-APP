05f16d8690f81d22a45027255df9876715d65508
emon999777 <alexster0012@gmail.com>
2025-03-26 19:46:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ app.get('/', (req, res) => {
       <body>
         <h1>আপনার ভিডিও লোড হচ্ছে, দয়া করে অপেক্ষা করুন...</h1>
         
-        <!-- এখানে আপনার ভিডিও বা অ্যাডের লিঙ্ক দিন -->
+        <!-- 
+    // Rewarded Popup
+
+show_9137000('pop').then(() => {
+    // user watch ad till the end or close it in interstitial format
+    // your code to reward user for rewarded format
+}).catch(e => {
+    // user get error during playing ad
+    // do nothing or whatever you want
+})
+
+         -->
         <script>
           setTimeout(() => {
             window.location.href = "
